d1487c531d97c9b98d73e469767e693a945f9894
xkluis <proffine1@gmail.com>
2020-11-24 11:47:13 -0600

    response json

--- iaserver3.py
+++ iaserver3.py
@@ -2,6 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 from io import BytesIO
 import stanza
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
 stanza.download('es') # download English model
 nlp = stanza.Pipeline('es') # initialize English neural pipeline
@@ -15,20 +18,30 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'Hello, world!')
 
     def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         jsonObject = json.loads(body)
         doc = nlp(jsonObject["text"]) # run annotation over a sentence
         words = []
+        responseDicc = {'permited':False,'words':[]}
         for sent in doc.sentences:
             for word in sent.words:
                 if word.upos == "ADJ":
                     words.append(word.text)
+        
+        df = pd.read_excel('dataset.xlsx')
+        for word in words:
+            if word in df['Insultos'].values:
+                responseDicc['permited'] = True
+        
+        responseDicc['words'] = words
 
-        self.send_response(200)
-        self.end_headers()
         response = BytesIO()
-        response.write(json.dumps(words).encode('utf-8'))
+        response.write(json.dumps(responseDicc).encode('utf-8'))
+        
         self.wfile.write(response.getvalue())
 
 
